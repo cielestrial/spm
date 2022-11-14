26bea0610b0aa2cee994c9c64e4a38d0d79b8bdb
cielestrial <32034862+cielestrial@users.noreply.github.com>
2022-11-13 23:14:55 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./css/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hello Spotify World</header>
+    </div>
+  );
+}
+
+export default App;
