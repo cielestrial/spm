bdba2f6621d9534e0d1a719cc26cc0617cc429e7
cielestrial <32034862+cielestrial@users.noreply.github.com>
2022-12-25 15:14:26 -0500

    serverless-http

--- server/main.cjs
+++ server/main.cjs
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const path = require("path");
 const express = require("express");
+const serverless = require("serverless-http");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const spotifyApi = require("./api/spotifyApi.cjs");
@@ -18,9 +19,9 @@ app.use(express.static(path.join(__dirname, "build")));
 app.set("port", process.env.PORT || 8080);
 
 const router = express.Router();
-app.use("/server", router);
+app.use("/.netlify/functions", router);
 
-app.get("/server", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/page.html"));
 });
 
@@ -28,56 +29,58 @@ app.get("/server", (req, res) => {
  * spotify api
  */
 // Get Access Token
-app.post("/server/login", spotifyApi.login);
+app.post("/login", spotifyApi.login);
 
 // Get UserId, display name, and country
-app.post("/server/user", spotifyApi.getUser);
+app.post("/user", spotifyApi.getUser);
 
 /**
  * spotify playlist api
  */
 // Get user's playlists
-app.post("/server/playlists", spotifyPlaylistApi.getPlaylists);
+app.post("/playlists", spotifyPlaylistApi.getPlaylists);
 
 // Create new playlist
-app.post("/server/create", spotifyPlaylistApi.create);
+app.post("/create", spotifyPlaylistApi.create);
 
 // Unfollow a playlist
-app.post("/server/unfollow", spotifyPlaylistApi.unfollow);
+app.post("/unfollow", spotifyPlaylistApi.unfollow);
 
 // Follow a playlist
-app.post("/server/follow", spotifyPlaylistApi.follow);
+app.post("/follow", spotifyPlaylistApi.follow);
 
 /**
  * spotify track api
  */
 // Add tracks to a specific position in a playlist
-app.post("/server/add", spotifyTrackApi.add);
+app.post("/add", spotifyTrackApi.add);
 
 // Remove all occurances of a track from a playlist
-app.post("/server/remove", spotifyTrackApi.remove);
+app.post("/remove", spotifyTrackApi.remove);
 
 // Get tracks in a playlist
-app.post("/server/tracks", spotifyTrackApi.getTracks);
+app.post("/tracks", spotifyTrackApi.getTracks);
 
 /**
  * spotify search api
  */
 // Search general playlists
-app.post("/server/search-playlists", spotifySearchApi.searchPlaylists);
+app.post("/search-playlists", spotifySearchApi.searchPlaylists);
 
 // Search general tracks
-app.post("/server/search-tracks", spotifySearchApi.searchTracks);
+app.post("/search-tracks", spotifySearchApi.searchTracks);
 
 /**
  * lastfm api
  */
 // Get associated genres for an artist
-app.post("/server/genres", lastfm.getArtistsGenres);
+app.post("/genres", lastfm.getArtistsGenres);
 
 // Reset associated genres for all artists
-app.post("/server/reset-genres", lastfm.resetArtistGenres);
+app.post("/reset-genres", lastfm.resetArtistGenres);
 
 /**
  * spotify liked api
  */
+
+module.exports.handler = serverless(app);
