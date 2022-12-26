820a9f527bba25726065bc572b4b361851ebbb5a
cielestrial <32034862+cielestrial@users.noreply.github.com>
2022-12-26 08:22:21 -0500

    nuked

--- src/api/functions/URI.tsx
+++
@@ -1,7 +0,0 @@
-const production = {
-  url: "https://yspm.vercel.app/",
-};
-const development = {
-  url: "http://localhost:3000",
-};
-export const envUri = development;
--- src/components/Nav/SearchBar/SearchBarTypes.tsx
+++
@@ -1,9 +0,0 @@
-import { playlistType, tracksType } from "../../../api/SpotifyApiClientTypes";
-
-export type dashboardRefType = {
-  setSelectedP: (selected: playlistType | undefined) => Promise<void>;
-  setSelectedT: (track: tracksType) => void;
-};
-
-export type searchCategoryType = "Playlists" | "Tracks";
-export type searchAreaType = "Library" | "General";
